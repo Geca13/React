46cece1b5aed961760e80d820a38ddd413071fad
Geca13 <karafiloski84@gmail.com>
2021-04-10 15:33:57 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from './tutorial/1-useState/setup/6-useState'
+import Setup from './tutorial/2-useEffect/setup/2-useEffect-cleanup'
 function App() {
   return (
     <div className='container'>
--- src/tutorial/2-useEffect/setup/1-useEffect-basics2.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics2.js
@@ -6,10 +6,10 @@ const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
   
   useEffect(()=>{
-    if(value>0) {
+    if(value>=1) {
   document.title=`new messages(${value})`;
     }
-  })
+  },[value]);
   return <>
   <h1>{value}</h1>
   <button className='btn' onClick={()=> setValue(value+1)}>click me</button>
