5ffd5732fdb2eb52028548aa8b742975deb717dd
Geca13 <karafiloski84@gmail.com>
2021-04-14 16:18:15 +0200

    useReducer

--- src/tutorial/6-useReducer/setup/prvovideo.js
+++ src/tutorial/6-useReducer/setup/prvovideo.js
@@ -2,37 +2,30 @@ import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
-const reducer = (state, action) =>{
-  if(action.type === 'TESTING') {
-    return {...state,people: data,isModalOpen: true,modalContent: 'item added'}
-  }
- return state;
-}
-const defaultState = {
-  people: [],
-  isModalOpen:false,
-  modalContent:''
-}
+
 const Index = () => {
   const[name, setName] = useState('');
-  const[state, dispatch] = useReducer(reducer,defaultState)
+  const [people, setPeople] = useState(data);
+  const [showModal, setShowModal] = useState(false);
   const handleSubmit = (e) => {
    e.preventDefault();
    if(name) {
-    dispatch({type:'TESTING'})
+    setShowModal(true);
+    setPeople([...people, {id: new Date().getTime().toString(), name}]);
+    setName('');
    } else{
-    
+     setShowModal(true)
    }
   };
   return ( <>
-  {state.isModalOpen && <Modal modalContent={state.modalContent}/>}
+  {showModal && <Modal/>}
   <form onSubmit={handleSubmit} className='form'>
     <div>
       <input type='text' value={name} onChange={(e)=> setName(e.target.value)}/>
     </div>
  <button type='submit'>add person</button>
   </form>
-  {state.people.map((person)=>{
+  {people.map((person)=>{
     return <div key={person.id}>
     <h4>{person.name}</h4>
     </div>
