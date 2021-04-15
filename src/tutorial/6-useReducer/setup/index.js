424981a43ef5f4e2a1320d5aa82a61c05f1c52b0
Geca13 <karafiloski84@gmail.com>
2021-04-15 16:50:11 +0200

    reducer

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,13 +1,9 @@
 import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
+import { reducer }
 // reducer function
-const reducer = (state, action) =>{
-  if(action.type === 'TESTING') {
-    return {...state,people: data,isModalOpen: true,modalContent: 'item added'}
-  }
- return state;
-}
+
 const defaultState = {
   people: [],
   isModalOpen:false,
@@ -19,13 +15,19 @@ const Index = () => {
   const handleSubmit = (e) => {
    e.preventDefault();
    if(name) {
-    dispatch({type:'TESTING'})
+     const newItem = {id: new Date().getTime().toString(), name}
+    dispatch({type:'ADD_ITEM', payload: newItem })
+    setName('');
    } else{
-    
+    dispatch({type: 'NO_VALUE'})
    }
   };
+
+  const closeModal = () => {
+   dispatch({type:'CLOSE_MODAL'});
+  }
   return ( <>
-  {state.isModalOpen && <Modal modalContent={state.modalContent}/>}
+  {state.isModalOpen && <Modal closeModal={closeModal} modalContent={state.modalContent}/>}
   <form onSubmit={handleSubmit} className='form'>
     <div>
       <input type='text' value={name} onChange={(e)=> setName(e.target.value)}/>
@@ -33,8 +35,9 @@ const Index = () => {
  <button type='submit'>add person</button>
   </form>
   {state.people.map((person)=>{
-    return <div key={person.id}>
+    return <div key={person.id} className='item'>
     <h4>{person.name}</h4>
+    <button onClick={()=> dispatch({type: 'REMOVE_ITEM',payload: person.id})}>remove</button>
     </div>
   })}
   </>
---
+++ src/tutorial/6-useReducer/setup/reducer.js
@@ -0,0 +1,26 @@
+const reducer = (state, action) => {
+    if(action.type === 'ADD_ITEM') {
+      const newPeople = [...state.people, action.payload];
+      return {...state,
+        people: newPeople,
+        isModalOpen: true,
+        modalContent: 'item added'}
+    };
+    if(action.type === 'NO_VALUE') {
+     return {...state,
+       isModalOpen: true,
+        modalContent: 'You must enter a value'}
+    }
+    if(action.type === 'CLOSE_MODAL') {
+      return { ...state,
+      isModalOpen: false};
+    }
+   if (action.type === 'REMOVE_ITEM') {
+     const newPeople = state.people.filter((person) => person.id !== action.payload);
+      return {...state, people: newPeople,isModalOpen: true,
+      modalContent: 'item was removed'};
+  
+    }
+   throw new Error ('No Matching Action Type');
+  }
+  
